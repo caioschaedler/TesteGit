df0ae326faff9b6e79afe927f3f993201135620f
caioschaedler <caiohschaedler@icloud.com>
2024-09-19 19:37:21 -0300

    Initial commit

---
+++ src/br/edu/unisep/testegit/view/prinicpal.java
@@ -0,0 +1,10 @@
+package br.edu.unisep.testegit.view;
+
+import javax.swing.*;
+
+public class prinicpal {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+                "mensagem de teste");
+    }
+}
